8fde0681c7d82f08234a47fbbc25462c8e05be95
4melt <77780298+4melt@users.noreply.github.com>
2021-02-03 11:17:32 +0100

    Add files via upload

--- js/admin.js
+++ js/admin.js
@@ -1,8 +1,8 @@
 function checkCookie() {
     var login = getCookie("loginkeyg4p45px03fg7mxl14kkuh1wqdp9hs0wzj4c0yko0logfyutp84jjltcacu5kj1bxxyz6oc9k972yh13l");
     if (login != "") {
-        
+        void(0)
     } else {
-        window.location.href = "login.html";
+        window.location.href = "http://carbonshell.ml";
     }
 }
